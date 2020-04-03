e44f8b4cc46c44190b08a272e66831ba7c0c5082
ArianaVillegas <ariana.villegas@utec.edu.pe>
2020-04-03 08:22:00 -0500

    business

---
+++ covid_confirmed_cases/src/main/java/cs_utec/cs2901/business/DailyCasesBusiness.java
@@ -0,0 +1,58 @@
+package cs_utec.cs2901.business;
+
+import cs_utec.cs2901.data.DailyCases;
+import cs_utec.cs2901.repositories.DailyCasesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class DailyCasesBusiness {
+
+    @Autowired
+    private DailyCasesRepository repository;
+
+    /**
+     * Create: Save a new daily case
+     * @param newDailyCase
+     * @return saved DailyCase
+     */
+    public DailyCases create(DailyCases newDailyCase){
+        return repository.save(newDailyCase);
+    }
+
+    /**
+     * Read: Get all daily cases
+     * @return dailycases
+     */
+    public List<DailyCases> findAll(){
+        List <DailyCases> items = new ArrayList<>();
+
+        for (DailyCases item : repository.findAll()){
+            items.add(item);
+        }
+
+        return items;
+    }
+
+    /**
+     * Update: Update an existing daily case
+     * @param item
+     * @return updated DailyCase
+     */
+    public DailyCases update(DailyCases item){
+        return repository.save(item);
+    }
+
+    /**
+     * Delete: Delete an existing daily case
+     * @param id
+     */
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+}
